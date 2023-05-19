fb5476bc5822a05312c93d8661c5307cbdbf101d
white-grudov <danekbel.1@gmail.com>
2023-05-19 11:10:58 +0300

    Added tests for Chapter 5

--- src/include/05Move.h
+++ src/include/05Move.h
@@ -2,6 +2,10 @@
 
 #include <utility>
 
+/*
+ * Item 23: Understand std::move and std::forward
+ */
+
 enum class CtorInvoked
 {
     Default,
@@ -66,6 +70,10 @@ public:
     CorrectItemContainer(CorrectItemContainer&& other) : item_(std::move(other.item_)) {}
 };
 
+/*
+ * Item 24: Distinguish universal references from rvalue references
+ */
+
 // universal reference, lvalue is passed
 template<typename T>
 bool func(T&& param)
@@ -78,3 +86,120 @@ bool func(std::vector<T>&& param)
 {
     return true;
 }
+
+/*
+ * Item 25: Use std::move on rvalue references, std::forward on universal references
+ */
+
+class Person
+{
+    std::string name_;
+
+public:
+    Person(const std::string& name) : name_(name) {}
+    Person(const Person& other) : name_(other.name_) {}
+    Person(Person&& other) : name_(std::move(other.name_)) {}
+
+    Person& operator=(const Person& other)
+    {
+        name_ = other.name_;
+        return *this;
+    }
+    Person& operator=(Person&& other)
+    {
+        name_ = std::move(other.name_);
+        return *this;
+    }
+
+    template <typename T>
+    void setName(T&& name)
+    {
+        name_ = std::forward<T>(name);
+    }
+    const std::string& getName() const
+    {
+        return name_;
+    }
+};
+
+/*
+ * Item 26: Avoid overloading on universal references
+ * Item 27: Familiarize yourself with alternatives to overloading on universal references
+ */
+
+// Tag dispatch
+
+template<typename T>
+bool processImpl(T&& param, std::false_type)
+{
+    return false;
+}
+
+template<typename T>
+bool processImpl(T&& param, std::true_type)
+{
+    return true;
+}
+
+std::string nameFromIdx(int idx)
+{
+    return "nameFromIdx";
+}
+
+template<typename T>
+bool processStrOrNumeral(T&& param)
+{
+    return processImpl(std::forward<T>(param), std::is_integral<std::remove_reference_t<T>>());
+}
+
+enum class PersonCtorType
+{
+    Universal, Integral, Copy, Move
+};
+
+class PersonConstrained
+{
+    std::string name_;
+    PersonCtorType ctorIndicator_;
+
+public:
+    template<typename T, typename = std::enable_if_t<!std::is_base_of<PersonConstrained, std::decay_t<T>>::value && !std::is_integral<std::remove_reference_t<T>>::value>>
+    explicit PersonConstrained(T&& n) : name_(std::forward<T>(n))
+    {
+        ctorIndicator_ = PersonCtorType::Universal;
+    }
+
+    explicit PersonConstrained(int idx) : name_(nameFromIdx(idx))
+    {
+        ctorIndicator_ = PersonCtorType::Integral;
+    }
+
+    PersonConstrained(const PersonConstrained& other) : name_(other.name_)
+    {
+        ctorIndicator_ = PersonCtorType::Copy;
+    }
+    PersonConstrained(PersonConstrained&& other) : name_(std::move(other.name_))
+    {
+        ctorIndicator_ = PersonCtorType::Move;
+    }
+
+    PersonConstrained& operator=(const PersonConstrained& other)
+    {
+        name_ = other.name_;
+        return *this;
+    }
+    PersonConstrained& operator=(PersonConstrained&& other)
+    {
+        name_ = std::move(other.name_);
+        return *this;
+    }
+
+    const std::string& getName() const
+    {
+        return name_;
+    }
+    PersonCtorType getCtorIndicator() const
+    {
+        return ctorIndicator_;
+    }
+};
--- tst/05Move-Test.cpp
+++ tst/05Move-Test.cpp
@@ -45,3 +45,76 @@ TEST(MoveItem24, RvalueIsPassed)
 
     EXPECT_TRUE(func(std::move(v)));
 }
+
+TEST(MoveItem25, MoveForRvalueReference)
+{
+    Person person("Test");
+    Person movedPerson(std::move(person));
+
+    EXPECT_EQ(movedPerson.getName(), "Test");
+    EXPECT_EQ(person.getName(), "");
+}
+
+TEST(MoveItem25, ForwardForUniversalReference)
+{
+    Person person("");
+    person.setName("CStr");
+    EXPECT_EQ(person.getName(), "CStr");
+
+    person.setName(std::string("rvalueStr"));
+    EXPECT_EQ(person.getName(), "rvalueStr");
+
+    std::string lvalueStr("lvalueStr");
+    person.setName(lvalueStr);
+    EXPECT_EQ(person.getName(), "lvalueStr");
+    EXPECT_EQ(lvalueStr, "lvalueStr");
+}
+
+TEST(MoveItem27, TagDispatchForUniversalReference)
+{
+    std::string lvalueStr("lvalueStr");
+
+    EXPECT_FALSE(processStrOrNumeral(lvalueStr));
+    EXPECT_FALSE(processStrOrNumeral(std::string("rvalueStr")));
+    EXPECT_FALSE(processStrOrNumeral("cStr"));
+
+    EXPECT_TRUE(processStrOrNumeral(42));
+    EXPECT_TRUE(processStrOrNumeral(static_cast<short>(42)));
+}
+
+TEST(MoveItem27, ConstrainedUniversalReferenceTemplate)
+{
+    std::string lvalueStr("lvalueStr");
+
+    PersonConstrained personLvalue(lvalueStr);
+    EXPECT_EQ(personLvalue.getName(), "lvalueStr");
+    EXPECT_EQ(personLvalue.getCtorIndicator(), PersonCtorType::Universal);
+
+    PersonConstrained personRvalue(std::string("rvalueStr"));
+    EXPECT_EQ(personRvalue.getName(), "rvalueStr");
+    EXPECT_EQ(personRvalue.getCtorIndicator(), PersonCtorType::Universal);
+
+    PersonConstrained personCStr("cStr");
+    EXPECT_EQ(personCStr.getName(), "cStr");
+    EXPECT_EQ(personCStr.getCtorIndicator(), PersonCtorType::Universal);
+
+    PersonConstrained personInt(42);
+    EXPECT_EQ(personInt.getName(), "nameFromIdx");
+    EXPECT_EQ(personInt.getCtorIndicator(), PersonCtorType::Integral);
+
+    PersonConstrained personShort(static_cast<short>(42));
+    EXPECT_EQ(personShort.getName(), "nameFromIdx");
+    EXPECT_EQ(personShort.getCtorIndicator(), PersonCtorType::Integral);
+
+    PersonConstrained personForCopy("copy");
+    PersonConstrained copiedPerson(personForCopy);
+    EXPECT_EQ(copiedPerson.getName(), "copy");
+    EXPECT_EQ(personForCopy.getName(), "copy");
+    EXPECT_EQ(copiedPerson.getCtorIndicator(), PersonCtorType::Copy);
+
+    PersonConstrained personForMove("move");
+    PersonConstrained movedPerson(std::move(personForMove));
+    EXPECT_EQ(movedPerson.getName(), "move");
+    EXPECT_EQ(personForMove.getName(), "");
+    EXPECT_EQ(movedPerson.getCtorIndicator(), PersonCtorType::Move);
+}
